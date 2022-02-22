cdb33502092a8b429b832539b6ba20037269f268
Advitiay Anand <advitiay.a@gmail.com>
2022-02-23 03:47:02 +0530

    Add basic unit tests

--- app/src/test/java/com/github/adizcode/androiddrafts/ExampleUnitTest.kt
+++ app/src/test/java/com/github/adizcode/androiddrafts/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.github.adizcode.androiddrafts
 
-import org.junit.Test
-
+import android.widget.ImageView
 import org.junit.Assert.*
+import org.junit.Test
+import java.lang.Math.pow
+import kotlin.math.pow
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,35 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val result = getRandomNum(6)
+        assertTrue("The value of rollResult was not between 1 and 6", result in 1..6)
+    }
+
+    @Test
+    fun equals_number() {
+        assertEquals(10, 5 * 2)
+    }
+
+    @Test
+    fun not_equals() {
+        assertNotEquals(5, 2)
+    }
+
+    @Test
+    fun is_false() {
+        assertFalse(2 > 2.0.pow(2.0))
+    }
+
+    @Test
+    fun is_null() {
+        assertNull(null)
+    }
+
+    @Test
+    fun not_null() {
+        assertNotNull("Hi")
     }
 }
