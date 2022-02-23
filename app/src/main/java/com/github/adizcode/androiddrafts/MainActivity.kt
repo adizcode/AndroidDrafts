968a159cb8929239c4f0ae19912861e049c24e6d
Advitiay Anand <advitiay.a@gmail.com>
2022-02-24 02:42:36 +0530

    Add functionality

--- app/src/main/java/com/github/adizcode/androiddrafts/MainActivity.kt
+++ app/src/main/java/com/github/adizcode/androiddrafts/MainActivity.kt
@@ -2,10 +2,35 @@ package com.github.adizcode.androiddrafts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.adizcode.androiddrafts.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val costOfService = binding.costOfService.text.toString().toDoubleOrNull()
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 20.0
+            R.id.option_eighteen_percent -> 18.0
+            else -> 15.0
+        }
+
+        var tip = costOfService ?: 0 * tipPercentage / 100
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+
+        binding.tipAmount.text = tip.toString()
     }
 }
